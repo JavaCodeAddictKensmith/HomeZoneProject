864e9fc0737ee9379a8aaf99774ccf1ada6b1cc9
Kenneth <ajahk@activedgetechnologies.com>
2025-07-17 22:54:46 -0700

    fixes issues

---
+++ HomeZone.Services.AuthAPI/Service/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using HomeZone.Services.AuthAPI.Models;
+
+namespace HomeZone.Services.AuthAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser, IEnumerable<string> roles);
+    }
+}
